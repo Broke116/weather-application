21a357d4c898106712f1a2065d2722787b4680e9
Broke116 <ekiny018@gmail.com>
2016-11-20 17:12:33 +0300

    search operation additions

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var app = express();
 var error = [];
 var weatherData = {};
 app.locals.error = error;
-app.locals.weatherData = weatherData;
 
 var weather = new ForecastIo("c56fea1eb4c8abf4a602f8769f446d67");
 
@@ -38,6 +37,33 @@ app.get(/^\/(\d{5})$/, function(req, res, next){ // captures the specified zip c
         if(err) { next(); return; }
         res.json({ // send json object with express json method for response
             zipcode: zipcode,
+            timezone: data.timezone,
+            summary: data.currently.summary,
+            windSpeed: data.currently.windSpeed,
+            temperature: data.currently.temperature,
+            humidity: data.currently.humidity,
+            dailySummary: data.daily.summary
+        });        
+    });
+});
+
+app.get(/^\/(\w+)/, function(req, res, next){
+    var state = req.params[0];
+    var city = req.query.city;
+    var location = zipdb.citystate(city, state);
+
+    console.log(location);
+    
+    var latitude = location.latitude;
+    var longitude = location.longitude;
+
+    console.log("lat:" , latitude, " long: ", longitude);
+
+    weather.forecast(latitude, longitude, function(err, data){ // get weather with lat long parameters
+        if(err) { console.log(err.stack); next(); return; }
+        console.log("weather data: ", data);
+        res.json({ // send json object with express json method for response
+            zipcode: data.zipcode,
             timezone: data.timezone,
             summary: data.currently.summary,
             windSpeed: data.currently.windSpeed,
--- public/main.js
+++ public/main.js
@@ -1,7 +1,10 @@
 $(function() {
     var $h1 = $("#title");
     var $zip = $("input[name='zip']");
-    $("form").on("submit", function(event) {
+    var $state = $("input[name='state']");
+    var $city = $("input[name='city']");
+    $("#zipform").on("submit", function(event) {
+        document.getElementById('weatherResult').innerHTML = "";
         event.preventDefault();
         
         var zipCode = $.trim($zip.val());        
@@ -14,7 +17,48 @@ $(function() {
 
         request.done(function(data) {
             var result = document.getElementById('weatherResult');
-            result.innerHTML = '<p>' + data.temperature + '</p>';
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    result.innerHTML += '<ul><li><span>' 
+                                    + key + "</span>: " + data[key] + 
+                                '</li></ul>';
+                }
+            }            
+
+            var temperature = data.temperature;
+            $h1.html("It is " + temperature + "&#176; in " + zipCode + ".");
+        });
+        
+        request.fail(function() {
+            $h1.text("Error!");
+        });
+    });
+    $("#stateform").on("submit", function(event) {
+        document.getElementById('weatherResult').innerHTML = "";
+        event.preventDefault();
+        
+        var stateCode = $.trim($state.val());  
+        var city = $.trim($city.val());       
+        $h1.text("Loading...");
+
+        var request = $.ajax({
+            url: "/" + stateCode,
+            data: {
+                "city": city
+            },
+            dataType: "json"
+        });
+
+        request.done(function(data) {
+            var result = document.getElementById('weatherResult');
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    result.innerHTML += '<ul><li><span>' 
+                                    + key + "</span>: " + data[key] + 
+                                '</li></ul>';
+                }
+            }            
+
             var temperature = data.temperature;
             $h1.html("It is " + temperature + "&#176; in " + zipCode + ".");
         });
